46e861ad531df58601f3f6c41fa5f431c316e283
oshton.tsen <oshton.tsen@forgerock.com>
2022-01-20 01:09:49 -0800

    Add ability to obtain current active spark session from anywhere in the codebase

--- utilities/spark.py
+++ utilities/spark.py
@@ -4,8 +4,6 @@ from pyspark.sql import SparkSession
 
 def initialise_spark() -> SparkSession:
     """Initialises and returns a Spark Session based on configuration"""
-    # retrieve global spark session variable
-    # parse the configuration file
     spark_config = SparkConf()
 
     # update spark config
@@ -14,3 +12,7 @@ def initialise_spark() -> SparkSession:
     # build session
     spark = SparkSession.builder.appName('Titanic Classifier').config(conf=spark_config).getOrCreate()
     return spark
+
+
+def get_current_session():
+    return SparkSession.builder.getOrCreate()
