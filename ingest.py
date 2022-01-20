49566ae697fcb4b2f65c25c4a3e7fa5b320a1e73
oshton.tsen <oshton.tsen@forgerock.com>
2022-01-20 11:57:59 -0800

    Create a class for the ingest stage

--- ingest.py
+++ ingest.py
@@ -3,13 +3,14 @@ import constants as cst
 from utilities.spark import get_current_session
 
 
-def ingest():
-    """Read train and test from csv"""
-    session = get_current_session()
-    # session = SparkSession.builder.getOrCreate()
-    train_df = session.read.format("csv").option("header", "true").load(cst.TRAIN_DATA_PATH)
-    test_df = session.read.format("csv").option("header", "true").load(cst.TEST_DATA_PATH)
-    return train_df, test_df
+class Ingest:
+    @staticmethod
+    def ingest_data():
+        """Read train and test from csv"""
+        session = get_current_session()
+        train_df = session.read.format("csv").option("header", "true").load(cst.TRAIN_DATA_PATH)
+        test_df = session.read.format("csv").option("header", "true").load(cst.TEST_DATA_PATH)
+        return train_df, test_df
 
 
 def main():
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import constants as cst
 
-from ingest import ingest
+from ingest import Ingest
+from preprocessor import Preprocessor
 from utilities.spark import initialise_spark
 
 
 def main() -> None:
     spark = initialise_spark()
-    train_df, test_df = ingest()
+    train_df, test_df = Ingest.ingest_data()
     return train_df, test_df
 
 
